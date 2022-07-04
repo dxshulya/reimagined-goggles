bd189a70a2543c1402f09a0feac4208ce3394d21
dxshulya <daraonelove@yandex.ru>
2022-07-04 12:39:17 +0300

    header todo

--- src/components/common/Footer/index.js
+++ src/components/common/Footer/index.js
@@ -1,34 +1,15 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
-
-import {
-  MAIN_LINK,
-} from "../../../router/links";
 
 import "./style.scss";
+
+import Logo from "../../../img/LightLogo.png";
 
 function Footer() {
 
   return (
     <footer className="footer">
-      <div className="footer__body">
-        <div className="emailBlock">
-          Для связи с администрацией:{" "}
-        </div>
-        <div className="menu">
-          {/*<NavLink exact className="menu__elemen" to={MAIN_LINK}>*/}
-          {/*  Главная*/}
-          {/*</NavLink>*/}
-          {/*<NavLink exact className="menu__elemen">*/}
-          {/*  Теория*/}
-          {/*</NavLink>*/}
-          {/*<NavLink exact className="menu__elemen">*/}
-          {/*  Тесты*/}
-          {/*</NavLink>*/}
-          {/*<NavLink exact className="menu__elemen">*/}
-          {/*  Блог*/}
-          {/*</NavLink>*/}
-        </div>
+      <div className="footer__wrapper">
+          <img className="logo" src={Logo} alt="Logo" width="160" height="48"/>
       </div>
     </footer>
   );
--- src/components/common/Header/index.js
+++ src/components/common/Header/index.js
@@ -1,39 +1,46 @@
 import React from "react";
-import { NavLink} from "react-router-dom";
 
 import Logo from "../../../img/Logo.png";
+import Icon from "../../../img/icon.svg";
 
 import "./style.scss";
 
 function Header() {
 
-  return (
-    <header className="header">
-      <div className="header__body">
-        <img
-          src={Logo}
-          className="header__logo"
-          alt="Logo"
-          width="170"
-          height="50"
-        />
-        <div className="menu">
-          {/*<NavLink>*/}
-          {/*  Главная*/}
-          {/*</NavLink>*/}
-          {/*<NavLink>*/}
-          {/*  Теория*/}
-          {/*</NavLink>*/}
-          {/*<NavLink>*/}
-          {/*  Тесты*/}
-          {/*</NavLink>*/}
-          {/*<NavLink>*/}
-          {/*  Блог*/}
-          {/*</NavLink>*/}
-        </div>
-      </div>
-    </header>
-  );
+    return (
+        <header className="header">
+            <div className="header__wrapper">
+                <img className="logo" src={Logo} alt="Logo" width="128" height="39"/>
+                <div className="menu">
+                    <div className="menu__wrapper">
+                        <nav>
+                            <a href="" className="menu__element">Shop</a>
+                            <a href="" className="menu__element">About us</a>
+                            <a href="" className="menu__element">Our team</a>
+                            <a href="" className="menu__element">Buyer's guides</a>
+                            <a href="" className="menu__element">Categories</a>
+                        </nav>
+
+                        <button className="buy_btn">
+                            <div className="buy_btn__text">Buy template</div>
+                        </button>
+
+                        <button className="cart_btn">
+                            <div className="cart_btn__wrapper">
+                                <img src={Icon} className="icon"></img>
+                                <div className="cart_text">Cart</div>
+                                <div className= "oval">
+                                    <div className= "oval__counter">0</div>
+                                </div>
+                            </div>
+
+                        </button>
+                    </div>
+
+                </div>
+            </div>
+        </header>
+    );
 }
 
 export default Header;
